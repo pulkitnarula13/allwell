d6d8011690172594000e2f71d6259e799af4608f
Prabhjyot <knowprabhjyot@gmail.com>
2022-06-04 17:29:59 -0700

    setup mongodb server

--- server/app.js
+++ server/app.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
 const PORT = process.env.PORT || 8080;
+require('dotenv').config()
+
+app.use(cors());
+
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, (error) => {
+    if (error) {
+        console.log(`Error connecting the database ${error}`);
+    } else {
+        console.log(`Connected to Allwell Database`);
+    }
+})
+
+app.get('/', (req, res) => {
+    res.send('All Well API');
+})
 
 app.listen(PORT, () => {
     console.log(`All well server running at port ${PORT}`);
