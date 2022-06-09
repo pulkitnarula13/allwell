8d8bc37ef048ffaa33ae48dfa6af47b6b9d85fe3
Prabhjyot <knowprabhjyot@gmail.com>
2022-06-09 14:55:52 -0700

    indented firebase config

--- client/Firebase/firebase.js
+++ client/Firebase/firebase.js
@@ -1,18 +1,16 @@
 import * as firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAQxZaezkG44MhO-c95_YAZSKb7jorKeiw",
-  authDomain: "medico-allwell.firebaseapp.com",
-  projectId: "medico-allwell",
-  storageBucket: "medico-allwell.appspot.com",
-  messagingSenderId: "901462508905",
-  appId: "1:901462508905:web:087e8b01d945c9b82c29f8"
+  apiKey: 'AIzaSyAQxZaezkG44MhO-c95_YAZSKb7jorKeiw',
+  authDomain: 'medico-allwell.firebaseapp.com',
+  projectId: 'medico-allwell',
+  storageBucket: 'medico-allwell.appspot.com',
+  messagingSenderId: '901462508905',
+  appId: '1:901462508905:web:087e8b01d945c9b82c29f8',
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
 }
 
-if(!firebase.apps.length){
-
-firebase.initializeApp(firebaseConfig);
-
-}
-
-export {firebase}
+export {firebase};
