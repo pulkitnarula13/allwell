06557c087e147dcac7c4df530c07a05cf8458386
Prabhjyot <knowprabhjyot@gmail.com>
2022-06-09 20:41:24 -0700

    patient model updated

---
+++ server/models/patient.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const PatientSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  dob: {
+    type: Date,
+    required: true,
+  },
+  phoneNumber: {
+    type: Number,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  profilePicture: {
+    type: String,
+  },
+  healthNumber: {
+    type: Number,
+    required: true,
+  },
+  healthDocument: {
+    type: String,
+    required: true,
+  },
+  gender: {
+    type: String,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const Patient = mongoose.model("Patient", PatientSchema);
+
+exports.Patient = Patient;
