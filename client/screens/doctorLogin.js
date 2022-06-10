6049936cef476b7ab8c1e88c1999c3d4cbce5c89
Jasleenkaur1998 <91931824+Jasleenkaur1998@users.noreply.github.com>
2022-06-09 17:39:09 -0700

    basic boiler plate for doctor signup and login

---
+++ client/screens/doctorLogin.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const doctorLogin = () => {
+  return (
+    <View>
+      <Text>doctorLogin</Text>
+    </View>
+  )
+}
+
+export default doctorLogin;
