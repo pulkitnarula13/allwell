404e307eaaec23fe0b9ec2cecb27bfa3a488d5fb
Prabhjyot <knowprabhjyot@gmail.com>
2022-06-12 10:13:14 -0700

    change from react native cli to expo cli

---
+++ client/screens/Doctor-Login.js
@@ -0,0 +1,17 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const DoctorLogin = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Text>DoctorLogin</Text>
+    </View>
+  );
+};
+
+export default DoctorLogin;
