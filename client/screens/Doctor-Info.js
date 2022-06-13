07ced3dc814abeae57ccbe5dc39c528481d696ad
KapilThaman <92117060+KapilThaman@users.noreply.github.com>
2022-06-12 23:44:35 -0700

    Doctor Infor Screen

---
+++ client/screens/Doctor-Info.js
@@ -0,0 +1,16 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+
+const DoctorInfo = () => {
+  return (
+    <View
+      style={{
+       
+      }}>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default DoctorInfo;
