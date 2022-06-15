36a43365fb1e65eca65d4bedea6f1db51a38ed49
Prabhjyot <knowprabhjyot@gmail.com>
2022-06-15 11:00:56 -0700

    fixed bottom navigation

--- client/components/BottomNavigation.js
+++ client/components/BottomNavigation.js
@@ -1,36 +1,29 @@
-
-import * as React from 'react';
-import { BottomNavigation, Text } from 'react-native-paper';
-import Home from '../screens/Doctor-Home'
-import Connect from '../screens/Doctor-connect'
-import History from '../screens/Doctor-History'
-import Profile from '../screens/Doctor-Profile'
-
-
-
-
-
-
-
+import * as React from "react";
+import { BottomNavigation } from "react-native-paper";
+import Home from "../screens/Doctor-Home";
+import Connect from "../screens/Doctor-connect";
+import History from "../screens/Doctor-History";
+import Profile from "../screens/Doctor-Profile";
 
 const Bottomnavigation = () => {
   const [index, setIndex] = React.useState(0);
   const [routes] = React.useState([
-    { key: 'home', title: 'Home', focusedIcon: 'home', unfocusedIcon: 'Home'},
-    { key: 'connect', title: 'Connect', focusedIcon: 'album' },
-    { key: 'history', title: 'History', focusedIcon: 'clock' },
-    { key: 'profile', title: 'Profile', focusedIcon: 'profile', unfocusedIcon: 'bell-outline' },
+    { key: "home", title: "Home", icon: "home" },
+    { key: "connect", title: "Connect", icon: "account-plus" },
+    { key: "history", title: "History", icon: "briefcase" },
+    { key: "profile", title: "Profile", icon: "account" },
   ]);
 
   const renderScene = BottomNavigation.SceneMap({
     home: Home,
     connect: Connect,
     history: History,
-    profile:Profile
+    profile: Profile,
   });
 
   return (
-    <BottomNavigation barStyle={{ backgroundColor: '#DADADA' }}
+    <BottomNavigation
+      barStyle={{ backgroundColor: "#DADADA" }}
       navigationState={{ index, routes }}
       onIndexChange={setIndex}
       renderScene={renderScene}
