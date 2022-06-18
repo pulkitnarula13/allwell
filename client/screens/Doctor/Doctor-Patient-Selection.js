f003bb6c445ab3ba2d33e3594b7d6fed41f7407b
Prabhjyot <knowprabhjyot@gmail.com>
2022-06-18 13:31:42 -0700

    doctor patient flow select

--- client/screens/Doctor/Doctor-Patient-Selection.js
+++ client/screens/Doctor/Doctor-Patient-Selection.js
@@ -1,22 +1,41 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 import React from "react";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 const DoctorPatientSelection = () => {
   return (
     <View style={style.container}>
-      <Text>Doctor-Patient-Selection</Text>
+      <View>
+        <TouchableOpacity style={{ width: 316, height: 208, borderRadius: 20, display: "flex", justifyContent: "center", alignItems: "center" }}>
+          <Image
+            style={{ width: 112, height: 112, borderRadius: 4 }}
+            source={require("../../assets/adaptive-icon.png")}
+          />
+          <Text>I'm a Patient</Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <TouchableOpacity style={{ width: 316, height: 208, borderRadius: 20, display: "flex", justifyContent: "center", alignItems: "center" }}>
+          <Image
+            style={{ width: 112, height: 112, borderRadius: 4 }}
+            source={require("../../assets/adaptive-icon.png")}
+          />
+          <Text>I'm a Doctor</Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <Text>Already have an account ? </Text>
+      </View>
     </View>
   );
 };
 
-
 const style = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
-
 
 export default DoctorPatientSelection;
