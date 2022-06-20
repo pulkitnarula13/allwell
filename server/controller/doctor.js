6586bf91ad18dea2a8cf183ab961fd8e18921ab0
Prabhjyot <knowprabhjyot@gmail.com>
2022-06-19 17:17:55 -0700

    fixed model route doctor

--- server/controller/doctor.js
+++ server/controller/doctor.js
@@ -1,4 +1,4 @@
-const { Doctor } = require("../models/Doctor");
+const { Doctor } = require("../models/doctor");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { Address } = require("../models/address");
