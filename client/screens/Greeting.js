6122dcae10a7e8792b8bb48aff104a51e5d96842
Prabhjyot <knowprabhjyot@gmail.com>
2022-06-22 16:05:48 -0700

    google auth setup

--- client/screens/Greeting.js
+++ client/screens/Greeting.js
@@ -1,5 +1,6 @@
 import { View, Button, StyleSheet } from "react-native";
 import React from "react";
+import GoogleSignup from "../components/GoogleSignup";
 
 const Greeting = ({ navigation }) => {
   return (
@@ -8,6 +9,7 @@ const Greeting = ({ navigation }) => {
         title="Go to Doctor/Patient Selection"
         onPress={() => navigation.navigate("Doctor-Patient-Selection")}
       />
+      < GoogleSignup />
     </View>
   );
 };
