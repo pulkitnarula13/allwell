08a3720886fc98a974bbbaeef778eee8611da03f
Pulkit Narula <43206958+pulkitnarula13@users.noreply.github.com>
2022-06-28 06:30:19 -0700

    Added the swagger information

--- server/routes/doctor.js
+++ server/routes/doctor.js
@@ -22,14 +22,64 @@ const {
  */
 router.post("/register", registerDoctor);
 
+// Routes
+/**
+ * @openapi
+ * /login:
+ *   post:
+ *     description: Send user to api for doctor login
+ *     responses:
+ *       200:
+ *         description: return positive response
+ */
 router.post("/login", loginDoctor);
 
+// Routes
+/**
+ * @openapi
+ * /:
+ *   get:
+ *     description: Get all the registered doctors
+ *     responses:
+ *       200:
+ *         description: return positive response
+ */
 router.get("/", getDoctors);
 
+// Routes
+/**
+ * @openapi
+ * /doctor/:id:
+ *   put:
+ *     description: Update information of a doctor profile
+ *     responses:
+ *       200:
+ *         description: return positive response
+ */
 router.put("/:id", updateDoctor);
 
+// Routes
+/**
+ * @openapi
+ * /doctor/:id:
+ *   delete:
+ *     description: Delete the doctor using id from the database
+ *     responses:
+ *       200:
+ *         description: return positive response
+ */
 router.delete("/:id", deleteDoctor);
 
+// Routes
+/**
+ * @openapi
+ * /doctor/:id:
+ *   get:
+ *     description: Get information of a doctor using id
+ *     responses:
+ *       200:
+ *         description: return positive response
+ */
 router.get("/:id", getDoctorById);
 
 module.exports = router;
--- server/routes/patient.js
+++ server/routes/patient.js
@@ -10,17 +10,76 @@ const {
   getPatientById,
 } = require("../controller/patient");
 
-
+// Routes
+/**
+ * @openapi
+ * /register:
+ *   post:
+ *     description: Register a new patient
+ *     responses:
+ *       200:
+ *         description: return positive response
+ */
 router.post("/register", registerPatient);
 
+// Routes
+/**
+ * @openapi
+ * /login:
+ *   post:
+ *     description: Send user to api for patient login
+ *     responses:
+ *       200:
+ *         description: return positive response
+ */
 router.post("/login", loginPatient);
 
+// Routes
+/**
+ * @openapi
+ * /patient:
+ *   get:
+ *     description: Get list of the all the patients
+ *     responses:
+ *       200:
+ *         description: return positive response
+ */
 router.get("/", getPatients);
 
+// Routes
+/**
+ * @openapi
+ * /patient/:id:
+ *   put:
+ *     description: Update information of a patient in the database
+ *     responses:
+ *       200:
+ *         description: return positive response
+ */
 router.put("/:id", updatePatient);
 
+// Routes
+/**
+ * @openapi
+ * /patient/:id:
+ *   delete:
+ *     description: Deelete a patient profile using id
+ *     responses:
+ *       200:
+ *         description: return positive response
+ */
 router.delete("/:id", deletePatient);
 
+// Routes
+/**
+ * @openapi
+ * /patient/:id:
+ *   get:
+ *     description: Get information of the patient using id
+ *     responses:
+ *       200:
+ *         description: return positive response
+ */
 router.get("/:id", getPatientById);
 
 module.exports = router;
