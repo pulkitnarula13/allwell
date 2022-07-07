a1eacf58c52e4b29fc30b6df0b4f29ed58b938e8
Pulkit Narula <43206958+pulkitnarula13@users.noreply.github.com>
2022-07-07 06:00:14 -0700

    Updated calendar and time selector

--- client/screens/Doctor/Accept-Patient-Schedule.js
+++ client/screens/Doctor/Accept-Patient-Schedule.js
@@ -1,8 +1,11 @@
 import { View, Text, StyleSheet, FlatList, Image } from "react-native";
 import React, { useState } from "react";
 import { Button } from "react-native-paper";
-import { ScrollView } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
 import DateTimePicker from "@react-native-community/datetimepicker";
+import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
+
+
 
 const DATA = [
   {
@@ -52,13 +55,14 @@ const AcceptPatientSchedule = () => {
   // Date Time Picker 
   // (Ref:- https://github.com/react-native-datetimepicker/datetimepicker#usage)
   const [date, setDate] = useState(new Date());
+  const [time, setTime] = useState(date);
   const [mode, setMode] = useState('date');
-  const [show, setShow] = useState(false);
+  const [show, setShow] = useState(true);
 
   const onChange = (event, selectedDate) => {
     const currentDate = selectedDate;
-    setShow(false);
-    setDate(currentDate);
+    // setShow(false);
+    setTime(currentDate);
   };
 
   const showMode = (currentMode) => {
@@ -112,7 +116,7 @@ const AcceptPatientSchedule = () => {
         </View>
         {/* Inner view ends here */}
 
-        <View style={styles.viewSchedulerContainer}>
+        {/* <View style={styles.viewSchedulerContainer}>
           <View style={styles.viewDateTimeContainer}>
             <View style={styles.viewDate}>
               <Text style={styles.textDatePicker} onPress={showDatepicker}>Date Picker</Text>
@@ -136,6 +140,74 @@ const AcceptPatientSchedule = () => {
           </View>
 
           <Text style={styles.textSelected}>selected: {date.toLocaleString()}</Text>
+
+        </View> */}
+
+        <View style={styles.viewDateTimeContainer}>
+          <Text style={styles.txtDate}>Date</Text>
+          <Calendar 
+            style={{
+              // marginLeft: 40,
+              borderWidth: 1,
+              borderColor: 'gray',
+              // height: 350,
+              // width: '80%',
+            }}
+            selected={date}
+            onDayPress={day => {
+            setDate(day.dateString);
+          }}
+          enableSwipeMonths={true}
+          theme={{
+            backgroundColor: '#ffffff',
+            calendarBackground: '#ffffff',
+            textSectionTitleColor: '#b6c1cd',
+            textSectionTitleDisabledColor: '#d9e1e8',
+            selectedDayBackgroundColor: '#00adf5',
+            selectedDayTextColor: '#ffffff',
+            todayTextColor: '#00adf5',
+            dayTextColor: '#2d4150',
+            textDisabledColor: '#d9e1e8',
+            dotColor: '#00adf5',
+            selectedDotColor: '#ffffff',
+            arrowColor: 'orange',
+            disabledArrowColor: '#d9e1e8',
+            monthTextColor: 'blue',
+            indicatorColor: 'blue',
+            textDayFontFamily: 'monospace',
+            textMonthFontFamily: 'monospace',
+            textDayHeaderFontFamily: 'monospace',
+            textDayFontWeight: '300',
+            textMonthFontWeight: 'bold',
+            textDayHeaderFontWeight: '300',
+            textDayFontSize: 16,
+            textMonthFontSize: 16,
+            textDayHeaderFontSize: 16
+          }}
+          />
+
+          <View style={styles.viewDividerLine} />
+
+
+          <View style={styles.viewTimePicker}>
+          <Text style={styles.txtSelectedDate}>Select Time:</Text>
+          {show && (
+            <DateTimePicker  
+                value={time}
+                mode={'time'}
+                is24Hour={true}
+                onChange={onChange}
+              />
+          )}
+          </View>
+
+
+
+
+
+            
+
+
 
         </View>
 
@@ -164,6 +236,8 @@ const AcceptPatientSchedule = () => {
             Accept
           </Button>
         </View>
+
+
 
       </View>
     </ScrollView>
@@ -286,9 +360,15 @@ const styles = StyleSheet.create({
 
   viewDateTimeContainer: {
     display: "flex",
-    flexDirection: "row",
+    flexDirection: "coloumn",
     alignItems: "center",
     justifyContent: "space-around",
+    // padding: 20,
+    margin: 30,
+    borderWidth: 2,
+    borderColor: "D9D9D9",
+    borderRadius: 10,
+
   },
 
 
@@ -300,8 +380,35 @@ const styles = StyleSheet.create({
 
   textSelected: {
     display: "block",
-    
-  }
+  },
+  viewDividerLine: {
+    borderBottomColor: "black",
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    paddingTop: 10,
+    paddingBottom: 10,
+  },
+  txtDate: {
+    textAlign: "left",
+    fontSize: 18,
+    padding: 10,
+
+  },
+
+  txtTime: {
+    textAlign: "left",
+    fontSize: 18,
+    padding: 10,
+  },
+
+  txtSelectedDate: {
+    textAlign: "left",
+    fontSize: 18,
+    padding: 10,
+  },
+
+  viewTimePicker: {
+    paddingBottom: 20,
+  },
 
 });
 
