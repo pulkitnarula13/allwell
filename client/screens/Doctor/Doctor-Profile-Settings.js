7a6208a362094b138e2142b6e58fa78920176dcc
KapilThaman <92117060+KapilThaman@users.noreply.github.com>
2022-07-08 03:54:14 -0700

    logout in doctor flow

--- client/screens/Doctor/Doctor-Profile-Settings.js
+++ client/screens/Doctor/Doctor-Profile-Settings.js
@@ -11,7 +11,7 @@ const DoctorProfileSettings = ({ navigation }) => {
 
   const patientLogout = () => {
     logout();
-    navigation.navigate("Doctor-Login");
+    navigation.navigate("greeting");
   };
 
   return (
